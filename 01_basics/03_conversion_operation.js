a43d046d938801dc3172d2ae2f2f1e59dd9ef0de
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-17 18:51:14 +0000

    datatype

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -2,5 +2,10 @@ let score = "123abc"
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let number = 50
+
+let negvalue = +number 
+console.log(negvalue);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+
+
+// premitive or reference 
+
+// premitives 
+// String 
+// Number 
+// Boolean
+// null
+// undefined
+// Symbol 
+// BigInt
+
+// javascript is dynatic
+
+// reference 
+// Array []
+// Object {}
+// function(){}
+
+// let heros = ["ironman , captainamerica , spiderman"]
+// console.log(typeof heros)
+
+
+// let myfunction = function(){
+//     console.log("hello world")
+// }
+
+
+ 
+
