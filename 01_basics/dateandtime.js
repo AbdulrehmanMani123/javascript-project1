47284e5f8b0b804d56dc69dd6a9ecca9dc577d89
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-23 20:51:48 +0000

    work

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,6 @@
+// let date = new Date()
+
+// let mycreated = new Date()
+// console.log(mycreated.toString())
+// console.log(mycreated.toDateString())
+// console.log(mycreated.toLocaleDateString())
