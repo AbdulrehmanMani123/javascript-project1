a679c43bb143d16fa448d9a322bb0bd018042063
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-24 22:53:42 +0000

    commit

---
+++ basicsjs/arrays.js
@@ -0,0 +1,34 @@
+// let arr = [1,2,3,4,5,6,7,8,9]
+// console.log(arr);
+
+// arr.push()
+// arr.pop()
+// arr.unshift()
+// arr.shift()
+// console.log(arr.includes())
+// console.log(arr.indexOf())
+
+// let newarray = arr.join()
+// console.log(arr)
+// console.log(newarray)
+
+// let marvelheros = ["thor , spiderman, captainamerica"]
+// let dcheros = ["superman , batman, wonderwomen"]
+
+// marvelheros.push(dcheros)
+// console.log(marvelheros);
+
+// let newarr = marvelheros.concat(dcheros)
+// console.log(newarr)
+
+// let newarr = [...marvelheros, ...dcheros]
+// console.log(newarr);
+
+// let arr = []
+// let newarr = arr.flat(Infinity)
+
+// console.log(newarr);
+
+
+
+
