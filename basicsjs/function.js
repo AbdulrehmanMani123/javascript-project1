def815f06990894c2f70213ceea71c612f4ac216
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-28 23:16:26 +0000

    commit

--- basicsjs/function.js
+++ basicsjs/function.js
@@ -4,3 +4,18 @@
 
 // abc()  
 
+// function calculatecartprice(val1, val2, ...num1) {
+//     return num1
+// }
+
+// console.log(calculatecartprice())
+
+function one(){
+    let a = 100
+
+    function two(){
+        let website = "website"
+        console.log(a);
+    }
+}
+one()
