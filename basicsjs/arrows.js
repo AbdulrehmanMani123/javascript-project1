42588f3533b8c0bf884c6aeffee4084153243092
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-30 23:00:41 +0000

    commit

--- basicsjs/arrows.js
+++ basicsjs/arrows.js
@@ -1,8 +1,8 @@
-let user = {
-    username: "logan",
-    price: 100000000000000,
+// let user = {
+//     username: "logan",
+//     price: 100000000000000,
 
-    welcomemessage: function(){
-        console.log(`${this.username}, logan is a scammer`);
-    }
-}
+//     welcomemessage: function(){
+//         console.log(`${this.username}, logan is a scammer`);
+//     }
+// }
