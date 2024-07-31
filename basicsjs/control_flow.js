98b871109ac1675355b77e535ed337269f7d0d08
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-07-31 23:19:47 +0000

    commit

---
+++ basicsjs/control_flow.js
@@ -0,0 +1,14 @@
+// if 
+
+// condition if false it will not run
+// condition if true it will run
+
+// let condition = 2 === 2
+// if(condition)  {
+//     console.log(condition)
+// }
+// else {
+//     console.log("not equals to 2");
+// }
+
+// < , > , <= , >= , == , != , === 
