7fed27be5cfc5744a0da7532b28944f14474417d
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-08-21 22:32:30 +0000

    commit

---
+++ basicsjs/function.js
@@ -0,0 +1,28 @@
+// function userfunction(){
+//     console.log("nick");
+//     console.log("jeremy");
+// }
+
+// userfunction()
+
+// function username(name = "") {
+//     if(name === "tesla"){
+//         console.log("not WL name")
+//         return
+
+//     }
+//     console.log("true your name is WL")
+//     return name 
+// }
+// console.log(username("jeremy"))
+
+// let user = {
+//     name: "jeremy",
+//     monthlyincome: "10000000$"
+// }
+
+// function users(obj) {
+//     console.log(`name is ${obj.name} monthlyincome ${obj.monthlyincome}`)
+// }
+// console.log(users(user))
+
