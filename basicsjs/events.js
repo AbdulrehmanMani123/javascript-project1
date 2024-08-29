4db4d0dc79f1a0f3076c70b001edbea9cfad384d
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-08-29 21:19:03 +0000

    commit

---
+++ basicsjs/events.js
@@ -0,0 +1,19 @@
+// events in javascript 
+
+
+// a.addEventListener(parameter'click', parameterfunc, parameter3)
+
+    // document.querySelector('logo').addEventListener('click', function (e){
+    //  alert("a")
+    // })
+
+    // propagation
+
+    // Bubbling: Events start at the innermost element and bubble up to the outermost element.
+    // Capturing: Events start at the outermost element and capture down to the innermost element.
+
+    // event properties
+// type , timstamp , defaultprevented , target , toElement , scrElement , click
+// currenttarget , clientX , clientY , screenX , screenY , altkey , ctrlkey , keycode 
+
+
