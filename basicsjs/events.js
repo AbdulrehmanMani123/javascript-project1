7be23c9e2148813db678e167972212c382ff78f0
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-09-01 22:05:39 +0000

    commit

--- basicsjs/events.js
+++ basicsjs/events.js
@@ -17,3 +17,34 @@
 // currenttarget , clientX , clientY , screenX , screenY , altkey , ctrlkey , keycode 
 
 
+// let a = function(){
+//     document.querySelector('h1').innerHTML = "js"
+// }
+
+// let change = setTimeout(a, 2000)
+
+// document.querySelector('#button').addEventListener('click', function(){
+//     clearTimeout(change)
+// })
+
+
+// let a = function() {
+// document.querySelector('h1').innerHTML = "js";
+// };
+
+// let intervalId; // Store the interval ID for pausing
+
+// let startButton = document.querySelector('#buttonstart');
+// startButton.addEventListener('click', function() {
+// if (!intervalId) { // Start the interval if it's not running
+// intervalId = setInterval(a, 2000);
+// }
+// });
+
+// let pauseButton = document.querySelector('#buttonpause');
+// pauseButton.addEventListener('click', function() {
+// if (intervalId) { // Pause the interval if it's running
+// clearInterval(intervalId);
+// intervalId = null;
+// }
+// });
