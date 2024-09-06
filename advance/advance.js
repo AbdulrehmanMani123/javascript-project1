5ea6f0c2c86309de231e0cc062d6320ed455fcf6
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-09-06 22:42:28 +0000

    commit

---
+++ advance/advance.js
@@ -0,0 +1,12 @@
+// promises in javascript
+
+// let a = new Promise(function(resolve, reject){
+//     setTimeout(function() {
+//         console.log("a")
+//     },1000)
+//     resolve()
+// })
+// a.then(function() {
+//     console.log('js')
+// }) 
+
