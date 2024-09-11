b1ed82f89aed45c41ecfffbe00f7649b9b0c4ca7
AbdulrehmanMani123 <143347354+AbdulrehmanMani123@users.noreply.github.com>
2024-09-11 22:40:47 +0000

    commit

--- advance/objjavascript.js
+++ advance/objjavascript.js
@@ -1,3 +1,15 @@
+// Javascript objoriented 
+
+// Key Concepts:
+
+// Constructor: A special method that initializes a new object when it's created.
+// Methods: Functions defined within a class.
+// Properties: Variables defined within a class.
+// Inheritance: A mechanism where one class can inherit properties and methods from another class.
+// Polymorphism: The ability of objects of different types to be treated as if they were of the same type.
+
+
+// Constructor 
 // new()  Creates a new instance of an object from a constructor function.
 
 // its importand to add this in object oriented programming 
@@ -14,4 +26,3 @@
   
 //   const john = new Person('John Doe', 30);
 //   john.greet(); // Output: Hello, my name is John Doe. 
-  
